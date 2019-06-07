a6fef0b1bb453394483b17229873336d11c77d32
Masaharu Kato <anothat.alone@gmail.com>
2019-06-07 20:03:43 +0900

    created bstream

---
+++ src/cinary_in.cpp
@@ -0,0 +1,21 @@
+﻿#include "cinary.h"
+#include "bstream.h"
+#include <iostream>
+#include <fstream>
+
+using namespace Cinary;
+
+int main()
+{
+	InputBinaryStream ibs(std::shared_ptr<std::ifstream>(new std::ifstream("test.bin")));
+
+	int v1, v2, v3, v4;
+	double v5;
+	char v6, v7, v8, v9;
+
+	ibs >> v1 >> v2 >> v3 >> v4 >> v5 >> v6 >> v7 >> v8 >> v9;
+
+	std::cout << v1 << ' ' << v2 << ' ' << v3 << ' ' << v4 << ' ' << v5 << ' ' << v6 << ' ' << v7 << ' ' << v8 << ' ' << v9;
+
+	return 0;
+}
---
+++ src/type_utils.h
@@ -0,0 +1,30 @@
+namespace Cinary {
+
+	template<typename T, typename _ = void>
+	struct is_container : std::false_type {};
+
+	template<typename... Ts>
+	struct is_container_helper {};
+
+	template<typename T>
+	struct is_container<
+			T,
+			std::conditional_t<
+				false,
+				is_container_helper<
+				//	typename T::value_type,
+				//	typename T::size_type,
+				//	typename T::allocator_type,
+				//	typename T::iterator,
+				//	typename T::const_iterator,
+				//	decltype(std::declval<T>().size()),
+					decltype(std::declval<T>().begin()),
+					decltype(std::declval<T>().end()),
+				//	decltype(std::declval<T>().cbegin()),
+				//	decltype(std::declval<T>().cend())
+					>,
+				void
+				>
+			> : public std::true_type {};
+
+}
